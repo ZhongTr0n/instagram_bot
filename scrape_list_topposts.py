2a985667f16f39cfcbf3ec9030601e7e2c05835c
ZhongTr0n <bruno.bossaerts@gmail.com>
2019-03-25 21:25:56 +0800

    Add sql connection

--- scrape_list_topposts.py
+++ scrape_list_topposts.py
@@ -1,11 +1,9 @@
-import csv
 import re
-import numpy as np
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import mysql.connector
 
-import csv
 
 url = "https://www.instagram.com/explore/tags/shanghai/"
 r = requests.get(url).text                           # Go to website
@@ -30,3 +28,50 @@ print(list_pic_id)
 
 for i in list_pic_id:
     print(i)
+    
+
+# FEED TO SQL DATABASE
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="internet",
+  database="Instagram"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES (%s, %s, %s, %s, %s);"
+vals = (1000, 'Misses', 'Python', 'Shanghai', 'China')
+
+mycursor.execute(sql, vals)
+
+mydb.commit()
+
+
+sql_search = "SELECT COUNT(*) FROM Persons WHERE LastName = 'Miste';"
+mycursor.execute(sql_search)
+row = mycursor.fetchall()
+print(row[0][0])
+
+
+'''
+
+x = conn.cursor()
+x.execute("SELECT *  FROM anooog1")
+x.execute (" INSERT INTO anooog1 VALUES ('%s','%s') ", (188,90))
+row = x.fetchall()
+
+  '''
+
+
+
+
+
+
+
+
+
+
+
+
